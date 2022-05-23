8e86210998008bd3075b66cdf47c76b167b236cf
koreandrum97 <park04181@gmail.com>
2022-05-23 13:58:31 +0900

    sbatch script

--- SSM_SINGLE/run_G.sh
+++ SSM_SINGLE/run_G.sh
@@ -15,5 +15,5 @@ conda activate notebook
 
 
 date
-python train.py --dataset_root ../adobe_240 --checkpoint_dir ./checkpoint --channel R
+python train.py --dataset_root ../adobe_240 --checkpoint_dir_G ./checkpoint --channel R
 date
---
+++ SSM_SINGLE/run_R.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#SBATCH -J SSM_SINGLE # Job name
+#SBATCH --time=2-00:00:00
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+#SBATCH -p cas_v100_4 # GPU 선택
+#SBATCH --comment pytorch
+#SBATCH --nodes=1 # 노드 수
+#SBATCH --ntasks-per-node=40 # 프로세스 수
+#SBATCH --gres=gpu:4 # GPU 개수
+
+module purge
+module load gcc/8.3.0 cuda/10.2
+conda activate notebook
+
+
+date
+python train.py --dataset_root ../adobe_240 --checkpoint_dir_R ./checkpoint --channel R
+date
