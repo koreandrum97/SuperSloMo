dbc5f883eaea74b1405a4d3714392d9c59d82281
koreandrum97 <park04181@gmail.com>
2022-05-23 16:04:05 +0900

    fix sbatch script

--- SSM_RGBRG/run.sh
+++ SSM_RGBRG/run.sh
@@ -6,9 +6,9 @@
 #SBATCH -p cas_v100_4 # GPU 선택
 #SBATCH --comment pytorch
 #SBATCH --nodes=1 # 노드 수
-#SBATCH --ntasks-per-node=40 # 프로세스 수
-#SBATCH --gres=gpu:4 # GPU 개수
-
+#SBATCH --ntasks-per-node=4 # 노드 당 프로세스
+#SBATCH --cpus-per-task=10 # 프로세스 당 CPU core
+#SBATCH --gres=gpu:4 # 노드 당 GPU
 module purge
 module load gcc/8.3.0 cuda/10.2
 conda activate notebook
--- SSM_SINGLE/run_B.sh
+++ SSM_SINGLE/run_B.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-#SBATCH -J SSM_SINGLE # Job name
+#SBATCH -J SSM_SINGLE_B # Job name
 #SBATCH --time=2-00:00:00
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
 #SBATCH -p cas_v100_4 # GPU 선택
 #SBATCH --comment pytorch
 #SBATCH --nodes=1 # 노드 수
-#SBATCH --ntasks-per-node=40 # 프로세스 수
-#SBATCH --gres=gpu:4 # GPU 개수
+#SBATCH --ntasks-per-node=4 # 노드 당 프로세스
+#SBATCH --cpus-per-task=10 # 프로세스 당 CPU core
+#SBATCH --gres=gpu:4 # 노드 당 GPU
 
 module purge
 module load gcc/8.3.0 cuda/10.2
